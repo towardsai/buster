030e5c4ee5e52e95df1f0d93a94f2a13a753d1c9
Omar Solano <omar@designstripe.com>
2024-02-06 16:58:48 -0500

    Update OpenAI embedding model

--- buster/llm_utils/embeddings.py
+++ buster/llm_utils/embeddings.py
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
-def get_openai_embedding_constructor(client_kwargs: Optional[dict] = None, model: str = "text-embedding-ada-002"):
+def get_openai_embedding_constructor(client_kwargs: Optional[dict] = None, model: str = "text-embedding-3-large"):
     if client_kwargs is None:
         client_kwargs = {}
     client = OpenAI(**client_kwargs)
